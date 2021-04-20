cd5c1f49a818d0902827f4a5c36b8935c6f8277f
SLAVA <71036863+Watz20@users.noreply.github.com>
2021-04-20 18:41:23 +0300

    Calculator +-*/ sqrt

---
+++ Main.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.lesson8;
+
+//1. Реализовать вычисление выражения для операций (без учета приоритетов):
+//   умножение, сложение, вычитание, деление
+//2. Реализовать вычисление квадратного корня
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new CalculatorFrame();
+    }
+
+}
